1b83f9e58089bc51249cf7dacbe3705239a587d0
zhaotongxue <zhaotongxue@outlook.com>
2018-08-12 15:57:30 +0800

    Mainly Advised is cpp config json file

--- Test.cpp
+++ Test.cpp
@@ -1,28 +1,22 @@
 // #include <bits/stdc++.h>
 #include <algorithm>
+#include <cstdio>
 #include <iostream>
 #include <vector>
+#include <deque>
 using namespace std;
 int main()
 {
     char name[256], title[256];
-    /*
-    std::cout << "Please, enter your name: ";
-    std::cin.getline (name,256);
-    std::cout << "Please, enter your favourite movie: "; std::cin.getline
-    (title,256); std::cout << name << "'s favourite movie is " << title; int
-    arr[]={1,2,4,69,0,4,5,2,1}; for (int x:arr){
-        printf("%d\n", x);
-    }
-    printf("Hello,world!");
-    */
-    vector<int> *v = new vector<int>;
-    //  v->push_back(10);
-    v->push_back(2);
-    v->push_back(3);
-    for (int i = 0; i < v->size(); i++)
-    {
-        printf("%d \n", v->at(i));
+    //just type to test the speed of calng completement.
+    //clang dynamic 
+    vector<int>* v;
+    if(v->empty()) return 1;
+else return 0;
+system("pause");
+system("pause");
+for (int i = 0; i < v->size(); i++) {
+  printf("%d \n", v->at(i));
     }
     printf("%d", std::max(1, 2));
     system("pause");
