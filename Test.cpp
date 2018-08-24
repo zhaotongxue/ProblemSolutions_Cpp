6a04ba8eb62f4925aa7bc73d954e8f58ea646a10
zhaotongxue <zhaotongxue@outlook.com>
2018-08-24 15:47:17 +0800

    REMOVE SOME FILES

--- Test.cpp
+++ Test.cpp
@@ -1,24 +1,13 @@
-#include <algorithm>
-#include <cstdio>
-#include <deque>
 #include <iostream>
-#include <vector>
-#include <stack>
 #include <string>
 using namespace std;
-int main() {
-   /*
-    stack<int> s;
-    for(int i=0;i<10;i++){
-        s.push(i*3);
-        printf("%d ",s.top());
-    }
-    printf("\n");
-    s.pop();
-    for(int i=0;i<10;i++){
-        printf("%d ",s.top());
-    }
-    */
-   printf("%s",string(1,97));
-    system("pause");
+int main(int argc, char const *argv[])
+{
+    /* code */
+    string s="zhao";
+    string c="zhao";
+    cout<<(s==c)<<endl;
+    for(int i=0;i<=s.length();i++)
+    cout<<s.substr(0,i)<<":"<<s.substr(s.length()-i)<<endl;
+    return 0;
 }
